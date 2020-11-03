90000a116173fcb69d3c1ce9192e7026a3a2fd58
Regis Santos <rg3915@yahoo.com.br>
2020-11-03 14:50:04 -0300

    Refatorando a api.

--- consumindo-api.py
+++ consumindo-api.py
@@ -1,26 +1,24 @@
-import pandas as pd
 import requests
-import json
+
 
 class ListaDeRepositorios():
 
     def __init__(self, usuario):
-        self._usuario = usuario
+        self.usuario = usuario
 
     def requisicao_api(self):
-        resposta = requests.get(f'https://api.github.com/users/grazimelo/repos')
+        resposta = requests.get(f'https://api.github.com/users/{self.usuario}/repos')
         if resposta.status_code == 200:
             return resposta.json()
-        else:
-            return resposta.status_code
+        return []
 
     def lista_repositorios(self):
         dados_api = self.requisicao_api()
-        if type(dados_api) is not int:
-            for i in range(len(dados_api)):
-                print(dados_api[i]['name'])
-        else:
-            print(dados_api)
-            
-repositorio_to_list = ListaDeRepositorios('grazimelo')
-repositorio_to_list.lista_repositorios()
+        if dados_api:
+            for i, item in enumerate(dados_api, 1):
+                print(i, item.get('name'))
+
+
+if __name__ == '__main__':
+    repositorio_to_list = ListaDeRepositorios('grazimelo')
+    repositorio_to_list.lista_repositorios()
